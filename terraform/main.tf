e0b7ee3619a9fe816905fe0086c9912f9dd29dde
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-08-13 21:49:50 +0530

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "sonar_sg" {
+  name        = "sonar_sg"
+  description = "Allow SSH and HTTP for SonarQube"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "sonarqube" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.sonar_sg.id]
+
+  tags = {
+    Name = "SonarQube"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Existing AWS Key Pair name"
+}
+
+variable "vpc_id" {}
+
+variable "subnet_id" {}
