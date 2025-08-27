98319ba1397c5dc8d2a297e697bd0cdaefc15426
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-08-28 01:49:11 +0530

    new commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,13 +44,20 @@ resource "aws_security_group" "sonar_sg" {
 
 # EC2 instance for SonarQube
 resource "aws_instance" "sonarqube" {
-  ami           = "ami-0f918f7e67a3323f0"
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = data.aws_subnets.default_vpc.ids[0]  # first subnet in default VPC
-  vpc_security_group_ids = [aws_security_group.sonar_sg.id]
+  ami                         = "ami-0f918f7e67a3323f0"
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = data.aws_subnets.default_vpc.ids[0]
+  vpc_security_group_ids      = [aws_security_group.sonar_sg.id]
+
+  root_block_device {
+    volume_size           = 15         # Set root EBS volume size to 15 GB
+    volume_type           = "gp3"      # Recommended EBS type
+    delete_on_termination = true       # Deletes volume when instance is terminated
+  }
 
   tags = {
     Name = "SonarQube"
   }
 }
+
